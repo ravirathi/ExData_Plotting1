b96e33ebaca29e1f1dccfc7af5e82ba4d0036fee
ravirathi <ravirathi7@yahoo.co.in>
2015-04-12 12:24:34 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,12 @@
+# Loading dplyr package
+library(dplyr)
+data <-  read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+DataDf <- tbl_df(data)
+DataReq <-  filter(DataDf, Date == "1/2/2007" | Date == "2/2/2007")
+datetime <- strptime(paste(DataReq$Date, DataReq$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+png("plot3.png", width = 480, height = 480)
+plot(datetime, as.numeric(DataReq$Sub_metering_1), type="l", col ="black", xlab=" ", ylab="Energy Submetering")
+lines(datetime, as.numeric(DataReq$Sub_metering_2), type="l", col ="red")
+lines(datetime, as.numeric(DataReq$Sub_metering_3), type="l", col ="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
